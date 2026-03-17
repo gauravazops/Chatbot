f03dc2ec5295b2c35662f6bec24b2f2bcff9ffaa
gauravazops <82805996+gauravazops@users.noreply.github.com>
2026-03-17 23:04:10 +0100

    Refactor generate_response to accept llm as parameter

--- app_groq.py
+++ app_groq.py
@@ -10,6 +10,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+groq_api_key = "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"
+
+#Langsmith Tracking
+LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+os.environ['LANGSMITH_TRACING'] = "true"
+os.environ['LANGSMITH_API_KEY'] = "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"
+os.environ["LANGSMITH_PROJECT"]="Q&A Chatbot with GROQ"
 ## Prompt Template
 
 prompt = ChatPromptTemplate.from_messages(
@@ -19,8 +26,8 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-def generate_response(question,temperature,max_tokens):
-    llm=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key=groq_api_key)
+def generate_response(question,llm,temperature,max_tokens):
+    llm=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key="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")
     output_parser=StrOutputParser()
     chain = prompt|llm|output_parser
     answer=chain.invoke({'question':question})
@@ -36,10 +43,11 @@ max_tokens=st.sidebar.slider("Max Tokens",min_value=50,max_value=300,value=150)
 ## Main interface for user input
 st.write("Go ahead and ask any question")
 user_input=st.text_input("You:")
+llm=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key="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")
 
 
 if user_input:
-    response=generate_response(user_input,temperature,max_tokens)
+    response=generate_response(user_input,llm,temperature,max_tokens)
     st.write(response)
 else:
     st.write("Please ask your question")
